e6d2d4fa33903ae4547eb0b94accc2d3cd33cdd5
Mehadi Hasan <mehadi.hasan@goava.com>
2025-08-10 19:58:09 +0600

    Add footer text in center

--- components/footer.tsx
+++ components/footer.tsx
@@ -23,7 +23,7 @@ export function Footer() {
   return (
     <footer className="mt-32 flex w-full justify-center">
       <div className="border-t w-full max-w-7xl border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
-        <div className="flex flex-col items-center justify-between gap-6 md:flex-row">
+        <div className="flex justify-center">
           <p className="text-sm text-zinc-400 dark:text-zinc-500">
             &copy; {new Date().getFullYear()} Md. Mehadi Hasan. All rights reserved.
           </p>
